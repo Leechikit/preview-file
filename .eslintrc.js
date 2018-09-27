8758cd130101b1bed4164733713df8298e73ac3e
leechikit <leechikit0823@163.com>
2018-09-27 15:25:01 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/html-indent': [
+      'error',
+      2
+    ],
+    'vue/no-parsing-error': [2, { "x-invalid-end-tag": false }]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="main" class="app-main">
+    <keep-alive>
+      <router-view v-if="$route.meta.keepAlive"></router-view>
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepAlive"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<style scoped lang="scss">
+
+</style>
